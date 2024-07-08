d588a13068c35b3a91e50dc9e88a862f7f93cbe7
Francisco Zorat <franciscozorat@gmail.com>
2024-07-08 10:41:12 -0300

    info return type

--- src/types/internal.ts
+++ src/types/internal.ts
@@ -3,3 +3,18 @@ export type BaseOptions = {
     degree?: string;
     timeout?: number;
 }
+
+export type WeatherJSInfo = {
+    location_code: string;
+    location_name: string;
+    degree: string;
+    provider: {
+        name: string;
+        url: string;
+    },
+    coords: {
+        latitude: string;
+        longitude: string;
+    },
+    timezone: string;
+}
