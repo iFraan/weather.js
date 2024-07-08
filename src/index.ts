742084fa6f6ab9d17efad0763028acbb25e5ff35
Francisco Zorat <franciscozorat@gmail.com>
2024-07-08 10:37:28 -0300

    finish migration

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-// const axios = require('axios');
-// const {XMLParser} = require('fast-xml-parser');
-
 import axios from 'axios';
 import { XMLParser } from 'fast-xml-parser';
-import { BaseOptions } from './types';
+import { BaseOptions, WeatherJSInfo } from './types/internal';
+import { MSNForecast, MSNResponse, MSNWeather } from './types/external';
 
 const Parser = new XMLParser({
     ignoreAttributes: false,
@@ -14,46 +12,48 @@ const baseUrl = 'http://weather.service.msn.com/find.aspx?src=outlook&weadegreet
 
 class API {
 
-    lang?: string;
-    degree?: string;
-    timeout?: number;
+    lang: string;
+    degree: string;
+    timeout: number;
     city?: any;
-    _raw?: any;
+    _raw?: MSNResponse;
 
+    /**
+     * Use API.search instead.
+     * @param {BaseOptions} options 
+     * @private // idk if it does something outside of typescript, but there it is
+     */
     constructor(options: BaseOptions) {
         this.lang = options?.lang ?? 'en-US';
         this.degree = options?.degree ?? 'F';
         this.timeout = options?.timeout ?? 10_000; // 10 seconds
-        this._raw = {}
     }
 
     /**
      * Search a city or place and returns an instance of the class
      * @param {String} city Place to search
-     * @param {String} options.lang Optional language 
-     * @param {String} options.degree Either C o F
-     * @param {Number} options.timeout Default 10 seconds
+     * @param {BaseOptions} options
      * @returns API Instance
      */
     static async search(city: string, options: BaseOptions = {}) {
         if (!city) throw new Error('Please provide a city to search for')
-        const W = new API({ ...options });
+        const instance = new API({ ...options });
         try {
-            const res = await axios.get(baseUrl.replace('{degree}', W.degree).replace('{lang}', W.lang).replace('{city}', city), { timeout: W.timeout });
-            W._raw = Parser.parse(res.data);
-            W.city = W._raw?.weatherdata?.weather?.[0] || W._raw?.weatherdata?.weather;
+            const { data } = await axios.get(baseUrl.replace('{degree}', instance.degree).replace('{lang}', instance.lang).replace('{city}', city), { timeout: instance.timeout });
+            instance._raw = Parser.parse(data);
+            instance.city = instance._raw?.weatherdata?.weather?.[0] || instance._raw?.weatherdata?.weather;
         } catch (err) {
             if (err.message.includes('code 500')) throw new Error('Server Internal Error')
             throw new Error(err.message)
         }
-        if (!W.city) throw new Error(W._raw)
-        return W;
+        if (!instance.city) throw new Error(instance._raw as unknown as string) // raw is a string when erroing
+        return instance;
     }
     /**
      * Current
      * @returns current weather with observation time
      */
-    current() {
+    current(): MSNWeather["current"] {
         return this.city.current;
     }
 
@@ -61,37 +61,37 @@ class API {
      * Forecast
      * @returns next days weather forecast
      */
-    forecast() {
+    forecast(): MSNForecast {
         return this.city.forecast;
     }
     /**
      * Info
      * @returns next days weather forecast
      */
-    info() {
-        const C = this.city;
-        const INFO = {
-            location_code: C.weatherlocationcode,
-            location_name: C.weatherlocationname,
-            degree: C.degreetype,
+    info(): WeatherJSInfo {
+        const city = this.city;
+        const data = {
+            location_code: city.weatherlocationcode,
+            location_name: city.weatherlocationname,
+            degree: city.degreetype,
             provider: {
-                name: C.provider,
-                url: C.attribution
+                name: city.provider,
+                url: city.attribution
             },
             coords: {
-                latitude: C.lat,
-                longitude: C.long,
+                latitude: city.lat,
+                longitude: city.long,
             },
-            timezone: C.timezone
+            timezone: city.timezone
         };
 
-        return INFO;
+        return data;
     }
 
     /**
      * Raw
      */
-    get raw() { return this._raw; }
+    get raw(): MSNResponse | string { return this._raw; }
 }
 
 export default API;
